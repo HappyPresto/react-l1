8b2b44ecdd63b15e08e6206289752aa32a84c0e7
HappyPresto <darkstararekku@gmail.com>
2019-07-29 17:45:34 +0300

    Add files via upload
    created Article

---
+++ src/fixtures.js
@@ -0,0 +1,48 @@
+export const articles = [
+    {
+        "id": "56c027",
+        "date": "2016-06-09T15:03:23.000Z",
+        "title": "Commodo qui incididunt",
+        "text": "Inlabre Commodo qui incididunt Commodo qui incididunt Commodo qui incididunt",
+        "comments": [
+            {
+                "id": "qwerqwer",
+                "user": "Gilliam Underwood",
+                "text": "Velit anim deserunt elit est fugiat duis eiumod eu"
+            },
+            {
+                "id": "lkjhsdlfkg",
+                "user": "Dolly Franklyn",
+                "text": "Aliquip id nostrud adipisicing irure"
+            },
+            {
+                "id": "fghjfghjfghj",
+                "user": "Jodi Humphrey",
+                "text": "Non amet amet ut magrna culpa dolore"
+            }
+        ]
+    },
+    {
+        "id": "56c717",
+        "date": "2018-09-09T15:03:23.000Z",
+        "title": "Quis occaecat",
+        "text": "Quis fghju sjkgf trturi sdsnmd asdasd njthythj rewedwe vbvnb",
+        "comments": [
+            {
+                "id": "ytrytryrt",
+                "user": "Mickle Gilberg",
+                "text": "Velit anim deserunt elit est fugiat duis eiumod eu"
+            },
+            {
+                "id": "vcfghjyt",
+                "user": "Chicken Burger",
+                "text": "Filtor gihpt dfjkdjk trtnrn dwqhgehweg htytjhyt"
+            },
+            {
+                "id": "ertyery",
+                "user": "Chris Brown",
+                "text": "Non amet amet ut magrna culpa dolore"
+            }
+        ]
+    }
+]
