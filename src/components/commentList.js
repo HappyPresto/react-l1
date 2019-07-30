86806f97717c1ae7d640a612598e4c79727f2455
HappyPresto <darkstararekku@gmail.com>
2019-07-30 17:10:49 +0300

    Add files via upload
    work

--- src/components/commentList.js
+++ src/components/commentList.js
@@ -3,32 +3,25 @@ import Comment from './comment'
 import toggleOpen from '../decorators/toggleOpen'
 
 // export default class CommentsList extends Component {  (тогда убираем экспорт внизу)
-class CommentList extends Component {
-    static defaultProps = {
-        comments: []
-    }
+function CommentList({comments = [], isOpen, toggleOpen}) {
+    const text = isOpen ? 'Hide comments' : 'Show comments'
+    return (
+        <div>
+            <button onClick={toggleOpen} >{text}</button>
+            {getCommentBody({comments, isOpen})}
+        </div>
+    )
+}
 
-    render() {
-        const text = this.props.isOpen ? 'Hide comments' : 'Show comments'
-        return (
-            <div>
-                <button onClick = {toggleOpen}>{text}</button>
-                {this.getCommentBody()}
-            </div>
-        )
-    }
+function getCommentBody({comments, isOpen}) {
+    if (!isOpen) return null
+    if (!comments.length) return <p>No comments yet</p>
 
-    getCommentBody() {
-        const {comments, isOpen} = this.props
-        if (!isOpen) return null
-        if (!comments.length) return <p>No comments yet</p>
-
-        return (
-            <ul>
-                {comments.map(comment => <li key = {comment.id}><Comment comment = {comment}/></li>)}
-            </ul>
-        )
-    }
+    return (
+        <ul>
+            {comments.map(comment => <li key = {comment.id}><Comment comment = {comment}/></li>)}
+        </ul>
+    )
 }
 
 export default toggleOpen(CommentList)
