ba42cabd9ad51255142968ae542714aeb76cebc6
HappyPresto <darkstararekku@gmail.com>
2019-08-02 16:52:47 +0300

    hw is done

--- src/components/commentList.js
+++ src/components/commentList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import propTypes from 'prop-types'
 import Comment from './comment'
 import toggleOpen from '../decorators/toggleOpen'
-import CommentListForm from './commentListForm'
+import CommentForm from './commentListForm'
 
 // export default class CommentsList extends Component {  (тогда убираем экспорт внизу)
 function CommentList({comments = [], isOpen, toggleOpen}) {
@@ -11,7 +11,7 @@ function CommentList({comments = [], isOpen, toggleOpen}) {
         <div>
             <button onClick={toggleOpen} >{text}</button>
             {getCommentBody({comments, isOpen})}
-            <CommentListForm />
+            <CommentForm />
         </div>
     )
 }
--- src/components/commentListForm.js
+++ src/components/commentListForm.js
@@ -1,3 +1,65 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './commentListForm.css'
+
+class CommentForm extends Component {
+    static propTypes = {
+
+    }
+
+    state = {
+        user: '',
+        text: ''
+    }
+
+    render() {
+        return (
+            <form onSubmit = {this.handleSubmit}>
+                name: <input value = {this.state.user}
+                             onChange = {this.handleChange('user')}
+                             className = {this.getClassName('user')} />
+                comment: <input value = {this.state.text} 
+                                onChange = {this.handleChange('text')}
+                                className = {this.getClassName('text')} />
+                <input type = "submit" value = "submit" />
+            </form>
+        )
+    }
+
+    handleSubmit = ev => {
+        ev.preventDefault()
+        this.setState({
+            user: '',
+            text: ''
+        })
+    }
+
+    getClassName = type => this.state[type].length && this.state[type].length < limits[type].min 
+        ? 'form-input__error' : ""
+
+    handleChange = type => ev => {
+        const {value} = ev.target
+        if (value.length > limits[type].max) return
+        this.setState({
+            [type]: value
+        })
+    }
+}
+
+const limits = {
+    user: {
+        min: 5,
+        max: 15
+    },
+    text: {
+        min: 20,
+        max: 50
+    }
+}
+
+export default CommentForm
+
+/*
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
@@ -61,3 +123,4 @@ class CommentListForm extends Component {
 }
 
 export default CommentListForm
+*/
